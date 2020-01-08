252be3059b700b1adf09ba87566fd027ea76364b
Nakajima-Kuro <tnnamanh@gmail.com>
2020-01-08 08:59:04 +0700

    Text preprocessing

--- model.py
+++ model.py
@@ -1,25 +1,42 @@
+#%%Import Lib
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
+from keras.layers.embeddings import Embedding
 from keras.models import Sequential, load_model
-from keras.layers import Dense
 from keras.optimizers import Adam
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+from keras.callbacks import ModelCheckpoint
+from keras.utils import to_categorical
+
 import glob
 import numpy as np
-from keras.utils import to_categorical
 import _pickle as pickle
 from sklearn.model_selection import train_test_split
-from keras.callbacks import ModelCheckpoint
+from clean_text import clean
 
 #%% Import Data
 nX = []
 ny = []
 Good = ['Dataset/NHuy/Good/*.txt','Dataset/NAnh/Good/*.txt','Dataset/Long/Good/*.txt']
-
+Bad = ['Dataset/NHuy/Bad/*.txt','Dataset/NAnh/Bad/*.txt','Dataset/Long/Bad/*.txt']
+##Import Good comment
 for link in Good:
     print(link)
     for filename in glob.glob(link):
         comment = open(filename, "r")
-        nX.append(comment.read())
-        ny.append(1)
+        nX.append(clean(comment.read()))
+        ny.append("Good")
         print(len(ny))
+##Import Bad comment
+for link in Bad:
+    print(link)
+    for filename in glob.glob(link):
+        comment = open(filename, "r")
+        nX.append(clean(comment.read()))
+        ny.append("Bad")
+        print(len(ny))
+del filename
+del link
+del Bad
+del Good
 print("Import successedfully")
