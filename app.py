e21a2fb87e1796561e9011b2e9736ac403aabdb7
Ngoc Huy <ngngochuy1107@gmail.com>
2020-01-10 17:15:14 +0700

    fix port

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def home():
     #     return(render_template('index.html', result = y))
     return render_template('index.html')
 if __name__ ==  '__main__':
-    app.run(host='0.0.0.0',port=port)
+    app.run(port=port)
