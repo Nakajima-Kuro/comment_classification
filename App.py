62f3b175154e94ec133b84ff2b81a9884c57aae1
Ngoc Huy <ngngochuy1107@gmail.com>
2020-01-10 13:34:05 +0700

    Preprocess + Clean text + predict in WebApi

--- App.py
+++ App.py
@@ -1,16 +1,24 @@
+from clean_text import clean
 from flask import render_template
 from flask import Flask,request
 from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
 app=Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.method == 'POST':
         data=request.form["data"]   #This is the data for prediction
+        data = clean(data)
+        vocabulary_size = 20000
+        tokenizer = Tokenizer(num_words= vocabulary_size)
+        tokenizer.fit_on_texts(data)
+        sequences = tokenizer.texts_to_sequences(data)
+        data = pad_sequences(sequences, maxlen=200)
         model = load_model('BestModel.hdf5')
-
+        y = model.predict(data).flatten()
     return render_template('index.html')
-
-
 if __name__ ==  '__main__':
     app.run()
